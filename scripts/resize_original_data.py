ea63f09d4349e6e822b166ee447796ce3878e37a
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-13 22:18:59 +0900

    Add resize.py

---
+++ scripts/resize_original_data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import os
+from skimage.io import imread
+from skimage.io import imsave
+from skimage.transform import resize
+
+from sklearn.datasets import load_files
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+if not os.path.exists(os.path.join(here, 'resized')):
+    print('Please copy original/ to resized/.')
+    quit()
+
+
+raw_files = load_files('resized/raw', shuffle=False, load_content=False)
+mask_files = load_files('resized/raw', shuffle=False, load_content=False)
+for rf, mf in zip(raw_files.filenames, mask_files.filenames):
+    assert os.path.basename(rf) == os.path.basename(mf)
+    raw_img = imread(rf)
+    raw_img = resize(raw_img, (712, 1068))
+    imsave(rf, raw_img)
+    mask_img = imread(mf)
+    mask_img = resize(mask_img, (712, 1068))
+    imsave(mf, mask_img)

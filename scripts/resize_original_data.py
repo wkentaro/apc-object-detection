b46ac0dd43bf4e5bf326c9c677c43a34247a24fb
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-13 23:50:35 +0900

    add resize.py

--- scripts/resize_original_data.py
+++ scripts/resize_original_data.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 
 import os
-from skimage.io import imread
+import cv2
 from skimage.io import imsave
 from skimage.transform import resize
 
@@ -20,9 +20,9 @@ if not os.path.exists(os.path.join(here, 'resized')):
 raw_files = load_files('resized/raw', shuffle=False, load_content=False)
 mask_files = load_files('resized/mask', shuffle=False, load_content=False)
 for rf, mf in zip(raw_files.filenames, mask_files.filenames):
-    raw_img = imread(rf)
+    raw_img = cv2.imread(rf)
     raw_img = resize(raw_img, (712, 1068))
     imsave(rf, raw_img)
-    mask_img = imread(mf)
+    mask_img = cv2.imread(mf)
     mask_img = resize(mask_img, (712, 1068))
     imsave(mf, mask_img)

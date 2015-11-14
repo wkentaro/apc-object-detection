49a484f3d7d11b45639a2fb42c5303cb66a02213
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-14 18:59:27 +0900

    Classnize unsupervised_train

--- scripts/unsupervised_train.py
+++ scripts/unsupervised_train.py
@@ -19,21 +19,6 @@ from draw_loss import draw_loss_curve
 
 
 here = osp.dirname(osp.abspath(__file__))
-TIMESTAMP = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-LOG_DIR = osp.join(here, '../logs/{}_unsupervised_train'.format(TIMESTAMP))
-os.mkdir(LOG_DIR)
-
-log_file = osp.join(LOG_DIR, 'log.txt')
-logging.basicConfig(
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    filename=log_file,
-    level=logging.DEBUG,
-)
-msg = 'logging at {}'.format(TIMESTAMP)
-logging.info(msg)
-print(msg)
-
-ON_GPU = True
 
 
 def im_preprocess(im):
@@ -54,72 +39,78 @@ def im_to_blob(im):
     return blob
 
 
-def train(dataset, model):
-    files = dataset.filenames
+class UnsupervisedTrain(object):
 
-    # fixed parameters
-    N = len(files)
-    batch_size = 10
-    adam_alpha = 0.001
-
-    # optimizer
-    optimizer = optimizers.Adam(alpha=adam_alpha)
-    optimizer.setup(model)
-
-    # train loop
-    sum_loss = 0
-    perm = np.random.permutation(N)
-    for i in range(0, N, batch_size):
-        files_batch = files[perm[i:i + batch_size]]
-        x_batch = np.array([im_to_blob(im_preprocess(cv2.imread(f)))
-                            for f in files_batch])
-        if ON_GPU:
-            x_batch = cuda.to_gpu(x_batch.astype(np.float32))
-
-        optimizer.zero_grads()
-        loss, x_hat = model.forward(x_batch, train=True)
-        loss.backward()
-        optimizer.update()
-
-        sum_loss += float(loss.data)
-
-    return sum_loss, model, x_hat
-
-
-def main():
-    # fixed parameters
-    n_epoch = 10
-    save_interval = n_epoch // 10
-
-    # model
-    model = CAE()
-    if ON_GPU:
-        model.to_gpu()
-
-    dataset = get_raw(which_set='train')
-    N = len(dataset.filenames)
-
-    for epoch in xrange(0, n_epoch):
-        sum_loss, model, x_hat = train(dataset, model)
-
-        # logging
-        msg = 'epoch:{:02d};\ttrain mean loss={};'.format(epoch, sum_loss / N)
+    def __init__(self, on_gpu=True):
+        self.on_gpu = on_gpu
+        self.timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        # setup for logging
+        self.log_dir = osp.join(
+            here, '../logs/{}_unsupervised_train'.format(self.timestamp))
+        os.mkdir(self.log_dir)
+        self.log_file = osp.join(self.log_dir, 'log.txt')
+        logging.basicConfig(
+            format='%(asctime)s [%(levelname)s] %(message)s',
+            filename=self.log_file,
+            level=logging.DEBUG,
+        )
+        msg = 'logging at {}'.format(self.timestamp)
         logging.info(msg)
         print(msg)
+        # model
+        self.model = CAE()
+        if self.on_gpu:
+            self.model.to_gpu()
+        # optimizer
+        self.optimizer = optimizers.Adam(alpha=0.001)
+        self.optimizer.setup(self.model)
 
-        if epoch % save_interval == 0:
-            print('epoch:{:02d};\tsaving model and x_hat'.format(epoch))
-            model_path = osp.join(
-                LOG_DIR, 'CAE_{}.chainermodel.pkl'.format(epoch))
-            with open(model_path, 'wb') as f:
-                pickle.dump(model, f)
-            x_hat_path = osp.join(LOG_DIR, 'x_hat_{}.pkl'.format(epoch))
-            with open(x_hat_path, 'wb') as f:
-                pickle.dump(x_hat.data, f)
+    def batch_loop(self, dataset, train, batch_size=10):
+        files = dataset.filenames
+        N = len(files)
+        # train loop
+        sum_loss = 0
+        perm = np.random.permutation(N)
+        for i in range(0, N, batch_size):
+            files_batch = files[perm[i:i + batch_size]]
+            x_batch = np.array([im_to_blob(im_preprocess(cv2.imread(f)))
+                                for f in files_batch])
+            if self.on_gpu:
+                x_batch = cuda.to_gpu(x_batch.astype(np.float32))
+            self.optimizer.zero_grads()
+            loss, x_hat = self.model.forward(x_batch, train=train)
+            loss.backward()
+            self.optimizer.update()
+            sum_loss += float(loss.data)
+        return sum_loss, x_hat
 
-        draw_loss_curve(
-            log_file, osp.join(LOG_DIR, 'loss_curve.jpg'), no_acc=True)
+    def main_loop(self, n_epoch=10, save_interval=None):
+        save_interval = save_interval or (n_epoch // 10)
+        dataset = get_raw(which_set='train')
+        N = len(dataset.filenames)
+        for epoch in xrange(0, n_epoch):
+            sum_loss, x_hat = self.batch_loop(dataset, train=True)
+            # logging
+            msg = ('epoch:{:02d};\ttrain mean loss={};'
+                   .format(epoch, sum_loss / N))
+            logging.info(msg)
+            print(msg)
+            # save
+            if epoch % save_interval == 0:
+                print('epoch:{:02d};\tsaving model and x_hat'.format(epoch))
+                model_path = osp.join(
+                    self.log_dir, 'CAE_{}.chainermodel.pkl'.format(epoch))
+                with open(model_path, 'wb') as f:
+                    pickle.dump(self.model, f)
+                x_hat_path = osp.join(
+                    self.log_dir, 'x_hat_{}.pkl'.format(epoch))
+                with open(x_hat_path, 'wb') as f:
+                    pickle.dump(x_hat.data, f)
+        draw_loss_curve(self.log_file,
+                        osp.join(self.log_dir, 'loss_curve.jpg'),
+                        no_acc=True)
 
 
 if __name__ == '__main__':
-    main()
+    app = UnsupervisedTrain()
+    app.main_loop()

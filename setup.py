7c3cac7eb1a0a1b6506b195b3150be416d2220bd
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-16 11:34:01 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='apc_od',
+    version=__import__('apc_od').__version__,
+    packages=find_packages(),
+    long_description=open('README.rst').read(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/apc_od',
+    install_requires=open('requirements.txt').readlines(),
+    license='MIT',
+    keywords='machine-learning',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)

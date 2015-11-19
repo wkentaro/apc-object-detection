da822ab17d4da6795dbf47eb968d572a487a3dc4
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-19 21:31:31 +0900

    Add CAE encode/decode method

--- tests/apc_od_tests/models_tests/test_cae.py
+++ tests/apc_od_tests/models_tests/test_cae.py
@@ -9,7 +9,7 @@ from apc_od import im_to_blob
 from apc_od.models import CAE
 
 
-def test_cae():
+def test_cae_train():
     img = astronaut()
     x = np.array([im_to_blob(img)])
 
@@ -20,3 +20,19 @@ def test_cae():
     optimizer.zero_grads()
     loss, x_hat = model.forward(x)
     loss.backward()
+
+
+def test_cae_encode():
+    img = astronaut()
+    x = np.array([im_to_blob(img)])
+
+    model = CAE()
+    model.encode(x)
+
+
+def test_cae_decode():
+    img = astronaut()
+    x = np.array([im_to_blob(img)])
+
+    model = CAE()
+    model.decode(x)

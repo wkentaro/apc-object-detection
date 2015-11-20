222a3e3e789a18d4bdafcd28aa883b800e0839bb
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-20 21:45:27 +0900

    Add catkin_linear

--- apc_od/models/__init__.py
+++ apc_od/models/__init__.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from apc_od.models import cae
+from apc_od.models import cae_linear
 from apc_od.models import sda
 
 
 CAE = cae.CAE
+CAELinear = cae_linear.CAELinear
 StackedAutoEncoder = sda.StackedAutoEncoder

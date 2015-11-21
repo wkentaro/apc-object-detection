850bbb21849d71cc87f758745f7fbc2035f5c43d
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-21 22:52:35 +0900

    fix imports for cae_pool

--- apc_od/models/__init__.py
+++ apc_od/models/__init__.py
@@ -3,4 +3,11 @@
 
 from apc_od.models.cae import CAE
 from apc_od.models.cae_linear import CAELinear
+from apc_od.models.cae_pool import CAEPool
 from apc_od.models.sda import StackedAutoEncoder
+
+
+CAE = CAE
+CAELinear = CAELinear
+CAEPool = CAEPool
+StackedAutoEncoder = StackedAutoEncoder

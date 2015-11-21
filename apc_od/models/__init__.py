526acc67c86afcb71d5411e7f3f131ee477c6d87
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-21 22:24:17 +0900

    fix import problem

--- apc_od/models/__init__.py
+++ apc_od/models/__init__.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from apc_od.models import cae
+from apc_od.models import cae_pool
 from apc_od.models import cae_linear
 from apc_od.models import sda
 
 
 CAE = cae.CAE
+CAEPool = cae_pool.CAEPool
 CAELinear = cae_linear.CAELinear
 StackedAutoEncoder = sda.StackedAutoEncoder

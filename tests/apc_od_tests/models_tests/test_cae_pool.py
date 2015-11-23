e12ca0cbaf6bc6984b095f45270459010a81d32d
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-23 18:16:25 +0900

    Adapt CAEPool to new model design

--- tests/apc_od_tests/models_tests/test_cae_pool.py
+++ tests/apc_od_tests/models_tests/test_cae_pool.py
@@ -13,20 +13,21 @@ from apc_od.models import CAEPool
 def test_cae_pool_train():
     img = astronaut()
     x_data = np.array([im_to_blob(img)])
+    x = Variable(x_data, volatile=False)
 
     model = CAEPool()
     optimizer = optimizers.Adam()
     optimizer.setup(model)
 
     optimizer.zero_grads()
-    loss, x_hat = model.forward(x_data)
+    loss, x_hat = model(x)
     loss.backward()
 
 
 def test_cae_pool_encode():
     img = astronaut()
     x_data = np.array([im_to_blob(img)])
-    x = Variable(x_data, True)
+    x = Variable(x_data, volatile=True)
 
     model = CAEPool()
     model.encode(x)
@@ -35,7 +36,7 @@ def test_cae_pool_encode():
 def test_cae_pool_decode():
     img = astronaut()
     x_data = np.array([im_to_blob(img)])
-    x = Variable(x_data, True)
+    x = Variable(x_data, volatile=True)
 
     model = CAEPool()
     h = model.encode(x)

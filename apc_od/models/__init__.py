37cd667cb75b582ad6e47b76e5083aa412e2590a
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-25 17:12:35 +0900

    Fix flake8

--- apc_od/models/__init__.py
+++ apc_od/models/__init__.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from apc_od.models.cae import CAE
-from apc_od.models.cae_pool import CAEPool
 from apc_od.models.cae_ones import CAEOnes
+from apc_od.models.cae_pool import CAEPool
 
 
 CAE = CAE
-CAEPool = CAEPool
 CAEOnes = CAEOnes
+CAEPool = CAEPool

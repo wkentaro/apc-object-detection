ef37dcd0a499f9b3518274ae1f191987dbdd0d31
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-26 16:54:42 +0900

    Adaptive shape decision by property of outsize

--- apc_od/models/cae_ones.py
+++ apc_od/models/cae_ones.py
@@ -13,15 +13,14 @@ class CAEOnes(chainer.Chain):
 
     def __init__(self):
         super(CAEOnes, self).__init__(
-            conv1_1=L.Convolution2D(3, 64, 2, stride=2, pad=1),
-            conv1_2=L.Convolution2D(64, 3, 2, stride=2, pad=1),
-            linear1_1=L.Linear(2346, 1000),
+            conv1_1=L.Convolution2D(3, 64, 3, stride=2, pad=1),
+            conv1_2=L.Convolution2D(64, 16, 3, stride=2, pad=1),
+            linear1_1=L.Linear(11616, 1000),
             linear1_2=L.Linear(1000, 4),
             linear2_1=L.Linear(4, 1000),
-            linear2_2=L.Linear(1000, 2346),
-            deconv2_1=L.Deconvolution2D(3, 64, 2, stride=2, pad=1),
-            deconv2_2=L.Deconvolution2D(64, 3, 2, stride=2, pad=1,
-                                        outsize=(178, 267)),
+            linear2_2=L.Linear(1000, 11616),
+            deconv2_1=L.Deconvolution2D(16, 64, 3, stride=2, pad=1),
+            deconv2_2=L.Deconvolution2D(64, 3, 3, stride=2, pad=1),
         )
         self.name = 'cae_ones'
         self.y = None
@@ -42,10 +41,17 @@ class CAEOnes(chainer.Chain):
 
     def encode(self, x):
         h = x
+        # conv1_1
+        self.conv1_1_inshape = h.data.shape
         h = self.conv1_1(h)
+        # conv1_2
+        self.conv1_2_inshape = h.data.shape
         h = self.conv1_2(h)
-        h = F.max_pooling_2d(h, ksize=2, stride=2)
+        # pool1
+        self.pool1_inshape = h.data.shape
+        h = F.max_pooling_2d(h, ksize=3, stride=2)
         self.pool1_outshape = h.data.shape
+        # linears
         h = self.linear1_1(h)
         h = self.linear1_2(h)
         z = h
@@ -53,11 +59,18 @@ class CAEOnes(chainer.Chain):
 
     def decode(self, z):
         h = z
+        # linears
         h = self.linear2_1(h)
         h = self.linear2_2(h)
+        # unpool1
         h = F.reshape(h, self.pool1_outshape)
-        h = F.unpooling_2d(h, ksize=2, stride=2)
+        h = F.unpooling_2d(h, ksize=3, stride=2,
+                           outsize=self.pool1_inshape[-2:])
+        # deconv2_1
+        self.deconv2_1.outsize = self.conv1_2_inshape[-2:]
         h = self.deconv2_1(h)
+        # deconv2_2
+        self.deconv2_2.outsize = self.conv1_1_inshape[-2:]
         h = self.deconv2_2(h)
         y = h
         return y

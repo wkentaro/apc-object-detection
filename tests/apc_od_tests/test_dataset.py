5d8a7f34cd7b3901c27128f3d08e65b3a61fde98
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-30 04:41:30 +0900

    Remove test for get_raw

--- tests/apc_od_tests/test_dataset.py
+++ tests/apc_od_tests/test_dataset.py
@@ -7,16 +7,6 @@ from nose.tools import assert_equal
 import apc_od
 
 
-def test_get_raw():
-    train_data = apc_od.get_raw('train')
-    test_data = apc_od.get_raw('test')
-
-    N_train = len(train_data.filenames)
-    N_test = len(test_data.filenames)
-
-    assert_almost_equal(int(N_train / 0.8), int(N_test / 0.2))
-
-
 def test_raw_to_mask_path():
     raw_path = '/home/wkentaro/apc-od/data/raw_train/doll/N1_0.jpg'
     expected = '/home/wkentaro/apc-od/data/mask/doll/N1_0.jpg'

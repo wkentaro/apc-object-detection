2af0aebfac4e027ccf06722cdc89399f59da744e
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-30 04:27:01 +0900

    Test raw_to_mask_path

--- tests/apc_od_tests/test_dataset.py
+++ tests/apc_od_tests/test_dataset.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from nose.tools import assert_almost_equal
+from nose.tools import assert_equal
 
 import apc_od
 
@@ -14,3 +15,9 @@ def test_get_raw():
     N_test = len(test_data.filenames)
 
     assert_almost_equal(int(N_train / 0.8), int(N_test / 0.2))
+
+
+def test_raw_to_mask_path():
+    raw_path = '/home/wkentaro/apc-od/data/raw_train/doll/N1_0.jpg'
+    expected = '/home/wkentaro/apc-od/data/mask/doll/N1_0.jpg'
+    assert_equal(expected, apc_od.raw_to_mask_path(raw_path))

f1714bbaf7d507ff59cf98972fc0a65d49eb856e
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-29 15:02:43 +0900

    Rename VGG_mini_ABN.py to snake case

---
+++ apc_od/models/vgg_mini_abn.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# copied from https://github.com/mitmul/chainer-cifar10
+
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import Variable
+
+
+class VGG_mini_ABN(chainer.Chain):
+    """VGGnet for CIFAR-10"""
+
+    def __init__(self):
+        super(VGG_mini_ABN, self).__init__(
+            conv1_1=L.Convolution2D(3, 64, 3, stride=1, pad=1),
+            bn1_1=L.BatchNormalization(64, decay=0.9, eps=1e-5),
+            conv1_2=L.Convolution2D(64, 64, 3, stride=1, pad=1),
+            bn1_2=L.BatchNormalization(64, decay=0.9, eps=1e-5),
+
+            conv2_1=L.Convolution2D(64, 128, 3, stride=1, pad=1),
+            bn2_1=L.BatchNormalization(128, decay=0.9, eps=1e-5),
+            conv2_2=L.Convolution2D(128, 128, 3, stride=1, pad=1),
+            bn2_2=L.BatchNormalization(128, decay=0.9, eps=1e-5),
+
+            fc3=L.Linear(4096, 1024),
+            fc4=L.Linear(1024, 1024),
+            fc5=L.Linear(1024, 3)
+        )
+        self.name = 'VGG_mini_ABN'
+
+    def __call__(self, x, y):
+        h = F.relu(self.bn1_1(self.conv1_1(x)))
+        h = F.relu(self.bn1_2(self.conv1_2(h)))
+        h = F.max_pooling_2d(h, 2, stride=2)
+        h = F.dropout(h, ratio=0.25)
+
+        h = F.relu(self.bn2_1(self.conv2_1(h)))
+        h = F.relu(self.bn2_2(self.conv2_2(h)))
+        h = F.max_pooling_2d(h, 2, stride=2)
+        h = F.dropout(h, ratio=0.25)
+
+        h = F.dropout(F.relu(self.fc3(h)), ratio=0.5)
+        h = F.dropout(F.relu(self.fc4(h)), ratio=0.5)
+        h = self.fc5(h)
+
+        return F.softmax_cross_entropy(h, y), F.accuracy(h, y), h
---
+++ tests/apc_od_tests/models_tests/test_vgg_mini_abn.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from chainer import optimizers as O
+from chainer import Variable
+import numpy as np
+
+from skimage.transform import resize
+
+from apc_od.data import snack
+from apc_od import im_preprocess
+from apc_od import im_to_blob
+from apc_od.models import VGG_mini_ABN
+
+
+class TestVGG_mini_ABN(unittest.TestCase):
+
+    def setUp(self):
+        self.model = VGG_mini_ABN()
+        self.optimizer = O.Adam()
+        self.optimizer.setup(self.model)
+
+        img = snack()
+        img = resize(img, (128, 128), preserve_range=True)
+        x_data = np.array([im_to_blob(img)])
+        y_data = np.array([0, 0])
+        self.x = Variable(x_data)
+        self.y = Variable(y_data)
+
+    def test_train(self):
+        self.optimizer.zero_grads()
+        loss, accuracy, _ = self.model(self.x, self.y)
+        loss.backward()
+        self.optimizer.update()

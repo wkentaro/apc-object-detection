41c595bba40ae9a86bd41bb26e494376e8eb8f46
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-30 04:16:24 +0900

    mask_to_roi to __init__.py

--- apc_od/__init__.py
+++ apc_od/__init__.py
@@ -12,6 +12,7 @@ __version__ = '0.1'
 im_preprocess = apc_od.data.im_preprocess
 im_to_blob = apc_od.data.im_to_blob
 blob_to_im = apc_od.data.blob_to_im
+mask_to_roi = apc_od.data.mask_to_roi
 
 
 get_raw = apc_od.dataset.get_raw

f0fc3313dec008a4ff902dfe244c4aae74565239
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-12-03 21:37:21 +0900

    Reliable vgg_mini_abn

--- apc_od/models/vgg_mini_abn.py
+++ apc_od/models/vgg_mini_abn.py
@@ -27,37 +27,49 @@ class VGG_mini_ABN(chainer.Chain):
             conv3_2=L.Convolution2D(256, 256, 3, stride=1, pad=1),
             bn3_2=L.BatchNormalization(256, decay=0.9, eps=1e-5),
 
-            fc4=L.Linear(6400, 4096),
-            fc5=L.Linear(4096, 1024),
-            fc6=L.Linear(1024, 3)
+            fc4=L.Linear(6400, 512),
+            bn4=L.BatchNormalization(512),
+            fc5=L.Linear(512, 512),
+            bn5=L.BatchNormalization(512),
+            fc6=L.Linear(512, 3)
         )
         self.train = True
         self.name = 'VGG_mini_ABN'
         self.loss = None
         self.accuracy = None
         self.y = None
+        self.pred = None
 
-    def __call__(self, x, t):
+    def __call__(self, x, t=None):
         h = F.relu(self.bn1_1(self.conv1_1(x), test=not self.train))
+        h = F.dropout(h, ratio=0.3, train=self.train)
         h = F.relu(self.bn1_2(self.conv1_2(h), test=not self.train))
         h = F.max_pooling_2d(h, 3, stride=3)
-        h = F.dropout(h, ratio=0.25, train=self.train)
 
         h = F.relu(self.bn2_1(self.conv2_1(h), test=not self.train))
+        h = F.dropout(h, ratio=0.4, train=self.train)
         h = F.relu(self.bn2_2(self.conv2_2(h), test=not self.train))
         h = F.max_pooling_2d(h, 3, stride=3)
-        h = F.dropout(h, ratio=0.25, train=self.train)
 
         h = F.relu(self.bn3_1(self.conv3_1(h), test=not self.train))
+        h = F.dropout(h, ratio=0.4, train=self.train)
         h = F.relu(self.bn3_2(self.conv3_2(h), test=not self.train))
+        h = F.dropout(h, ratio=0.4, train=self.train)
         h = F.max_pooling_2d(h, 3, stride=3)
-        h = F.dropout(h, ratio=0.25, train=self.train)
 
-        h = F.dropout(F.relu(self.fc4(h)), ratio=0.5, train=self.train)
-        h = F.dropout(F.relu(self.fc5(h)), ratio=0.5, train=self.train)
+        h = F.dropout(h, ratio=0.5, train=self.train)
+        h = F.relu(self.bn4(self.fc4(h), test=not self.train))
+        h = F.dropout(h, ratio=0.5, train=self.train)
+        h = F.relu(self.bn5(self.fc5(h), test=not self.train))
+        h = F.dropout(h, ratio=0.5, train=self.train)
         h = self.fc6(h)
-
-        self.loss = F.softmax_cross_entropy(h, t)
-        self.accuracy = F.accuracy(h, t)
         self.y = h
-        return self.loss
+
+        if t is not None:
+            self.loss = F.softmax_cross_entropy(h, t)
+            self.accuracy = F.accuracy(h, t)
+            return self.loss
+
+        assert self.train == False
+        self.pred = F.softmax(self.y)
+        return self.pred
